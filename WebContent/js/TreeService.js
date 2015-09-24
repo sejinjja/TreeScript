a2c5035c587786276abc87b16e31b0e7fa80cecf
조세진 <sejinjja@naver.com>
2015-09-24 11:07:12 +0900

    ....실패...

--- WebContent/js/TreeService.js
+++ WebContent/js/TreeService.js
@@ -32,9 +32,67 @@ function upack(mayTA) {
 
 }
 
+function removeDuplicate(dataArr){
+	
+	var chekIdArr = [];
+	var tempArr = [];
+	
+	for(i = 0 ; i < dataArr.length ; i ++){
+		
+		//alert(chekIdArr.indexOf(dataArr[i].id));
+		//alert(chekIdArr.indexOf(dataArr[i].id) == -1);
+		if(chekIdArr.indexOf(dataArr[i].id) == -1){
+			chekIdArr.push(dataArr[i].id)
+			tempArr.push(dataArr[i])
+		}
+		
+	}
+	return tempArr;		
+	
+}
+
+//parent가 아닌것부터 부모를 찾아야 함.
+function makeTree(dataArr){
+	
+	var tempArr = [];
+	var chekIdArr = [];
+	
+	for(i = 0 ; i < dataArr.length ; i ++){
+		
+		if(chekIdArr.indexOf(dataArr[i].parent) == -1){
+			chekIdArr.push(dataArr[i].id);
+		}		
+		
+	}
+	
+	for(i = 0 ; i < dataArr.length ; i ++){
+		
+		if(chekIdArr.indexOf(dataArr[i].id) == -1){
+			
+			for(j = 0 ; j < dataArr.length ; j ++){
+				
+				if(dataArr[j].id == dataArr[i].parent){
+					dataArr[j].children.push(dataArr[i]);
+					//tempArr.push(dataArr[j]);
+					break;
+				}
+				
+			}
+			
+		}else{
+			tempArr.push(dataArr[i]);
+		}
+		
+	}
+	
+	return tempArr;	
+}
+
 //초기 화면 구성용
 function init(){
 	upack(list);
+	list = removeDuplicate(list);
+	list = makeTree(list);
 	display();
 }
 window.onload = init;
