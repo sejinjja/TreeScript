a10148d6775da2d60bd6d8474aa866bd35111d13
조세진 <sejinjja@naver.com>
2015-09-28 16:14:42 +0900

    Ver1 Tree

--- WebContent/js/SampleDataSource.js
+++ WebContent/js/SampleDataSource.js
@@ -1,94 +1,138 @@
 /**
  * sampleData
  */
+
+var list = [ {
+	"id" : 1,
+	"parent" : 0,
+	"children" : []
+}, {
+	"id" : 2,
+	"parent" : 1,
+	"children" : []
+}, {
+	"id" : 3,
+	"parent" : 1,
+	"children" : []
+}, {
+	"id" : 4,
+	"parent" : 2,
+	"children" : []
+}, {
+	"id" : 5,
+	"parent" : 2,
+	"children" : []
+}, {
+	"id" : 6,
+	"parent" : 0,
+	"children" : []
+}, {
+	"id" : 7,
+	"parent" : 0,
+	"children" : []
+}, {
+	"id" : 8,
+	"parent" : 7,
+	"children" : []
+}, {
+	"id" : 9,
+	"parent" : 8,
+	"children" : []
+}, {
+	"id" : 10,
+	"parent" : 0,
+	"children" : []
+} ];
 //(function(){
-var list = [
-{
-	"id": 1,
-	"parent": 0,
-	"children": [
-		{
-			"id": 2,
-			"parent": 1,
-			"children": [
-				{
-					"id": 3,
-					"parent": 1,
-					"children": []
-				},
-				{
-					"id": 4,
-					"parent": 2,
-					"children": []
-				}
-			]
-		},
-		{
-			"id": 3,
-			"parent": 1,
-			"children": []
-		}
-	]
-},
-{
-	"id": 2,
-	"parent": 1,
-	"children": []
-},
-{
-	"id": 3,
-	"parent": 1,
-	"children": []
-},
-{
-	"id": 4,
-	"parent": 2,
-	"children": []
-},
-{
-	"id": 5,
-	"parent": 2,
-	"children": []
-},
-{
-	"id": 6,
-	"parent": 0,
-	"children": []
-},
-{
-	"id": 7,
-	"parent": 0,
-	"children": []
-},
-{
-	"id": 8,
-	"parent": 7,
-	"children": []
-},
-{
-	"id": 9,
-	"parent": 8,
-	"children": []
-},
-{
-	"id": 10,
-	"parent": 0,
-	"children": []
-},
-{
-	"id": 8,
-	"parent": 7,
-	"children": []
-},
-{
-	"id": 9,
-	"parent": 8,
-	"children": []
-},
-{
-	"id": 10,
-	"parent": 0,
-	"children": []
-}
-];
+/*
+ var list = [
+ {
+ "id": 1,
+ "parent": 0,
+ "children": [
+ {
+ "id": 2,
+ "parent": 1,
+ "children": [
+ {
+ "id": 3,
+ "parent": 1,
+ "children": []
+ },
+ {
+ "id": 4,
+ "parent": 2,
+ "children": []
+ }
+ ]
+ },
+ {
+ "id": 3,
+ "parent": 1,
+ "children": []
+ }
+ ]
+ },
+ {
+ "id": 2,
+ "parent": 1,
+ "children": []
+ },
+ {
+ "id": 3,
+ "parent": 1,
+ "children": []
+ },
+ {
+ "id": 4,
+ "parent": 2,
+ "children": []
+ },
+ {
+ "id": 5,
+ "parent": 2,
+ "children": []
+ },
+ {
+ "id": 6,
+ "parent": 0,
+ "children": []
+ },
+ {
+ "id": 7,
+ "parent": 0,
+ "children": []
+ },
+ {
+ "id": 8,
+ "parent": 7,
+ "children": []
+ },
+ {
+ "id": 9,
+ "parent": 8,
+ "children": []
+ },
+ {
+ "id": 10,
+ "parent": 0,
+ "children": []
+ },
+ {
+ "id": 8,
+ "parent": 7,
+ "children": []
+ },
+ {
+ "id": 9,
+ "parent": 8,
+ "children": []
+ },
+ {
+ "id": 10,
+ "parent": 0,
+ "children": []
+ }
+ ];
+ */
 //})();
--- WebContent/js/TreeService.js
+++ WebContent/js/TreeService.js
@@ -2,6 +2,8 @@
  * 
  */
 
+var test;
+var test2;
 //태그에 현재 리스트 출력
 function display() {
 	document.getElementById("tree").innerHTML = JSON
@@ -54,6 +56,61 @@ function removeDuplicate(dataArr){
 //parent가 아닌것부터 부모를 찾아야 함.
 function makeTree(dataArr){
 	
+	var isParentArr = [];
+	var delList = [];
+	var idDelList = [];
+	//var chekIdArr = [];
+	
+	for(i = 0 ; i < dataArr.length ; i ++){
+		if(isParentArr.indexOf(dataArr[i].parent) == -1){
+			isParentArr.push(dataArr[i].parent);
+		}
+	}
+
+	isParentArr.pop(0);
+	//isParentArr 부모들
+
+	for(i = 0 ; i < dataArr.length ; i ++){
+		if(isParentArr.indexOf(dataArr[i].parent) != -1){//부모가 아닌 인덱스만.
+			for(j = 0 ; j < dataArr.length ; j ++){//부모 찾기
+				if(dataArr[i].id == dataArr[j].parent){
+					//alert("dataArr[i].id" + dataArr[i].id  + "," + "dataArr[i].parent" +","+ dataArr[i].parent + "\n"
+					//		+"dataArr[j].id" + dataArr[j].id  + "," + "dataArr[j].parent" +","+ dataArr[j].parent);  
+					//dataArr[i].children.push(dataArr.pop(dataArr[j]));
+					dataArr[i].children.push(dataArr[j]);
+					delList.push(dataArr[j]);
+					
+				}
+			}			
+		}
+	}
+
+	for(i = delList.length -1 ; i > -1  ; i --){
+		//alert(dataArr.indexOf(delList[i]));
+		//idDelList.push(dataArr.pop(dataArr.indexOf(delList[i])));
+		//dataArr.pop(dataArr.indexOf(delList[i]));
+		if(dataArr.indexOf(delList[i])){
+			list.splice(dataArr.indexOf(delList[i]), 1);
+		}
+			
+		//test2 = idDelList;
+		//dataArr.pop(dataArr.indexOf(delList[i]));
+	}
+	
+	test2 = idDelList;
+	test = delList;
+	for(i = 0 ; i < dataArr.length ; i ++){
+		if(dataArr.indexOf(dataArr[i].parent) != -1){
+			
+			//alert(dataArr[i].children[0].id);
+		}
+	}
+	
+	
+	
+	
+	/*
+	
 	var tempArr = [];
 	var chekIdArr = [];
 	
@@ -83,16 +140,16 @@ function makeTree(dataArr){
 			tempArr.push(dataArr[i]);
 		}
 		
-	}
+	}*/
 	
-	return tempArr;	
+	return dataArr;	
 }
 
 //초기 화면 구성용
 function init(){
-	upack(list);
+	//upack(list);
 	list = removeDuplicate(list);
-	list = makeTree(list);
+	list = makeTree(list);	
 	display();
 }
 window.onload = init;
