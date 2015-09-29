25a7d4ee39fcf0282bc660d94c4097cac87319f7
조세진 <sejinjja@naver.com>
2015-09-29 19:30:26 +0900

    StringID

--- WebContent/js/SampleDataSource2.js
+++ WebContent/js/SampleDataSource2.js
@@ -1,48 +1,46 @@
-
-
 /**
  * sampleData
  */
 
 var list = [ {
-	"id" : 1,
+	"id" : "a",
+	"parent" : 0,	
+	"children" : []
+}, {
+	"id" : "ab",
+	"parent" : "a",
+	"children" : []
+}, {
+	"id" : "abc",
+	"parent" : "ab",
+	"children" : []
+}, {
+	"id" : "abd",
+	"parent" : "ab",
+	"children" : []
+}, {
+	"id" : "b",
 	"parent" : 0,
 	"children" : []
 }, {
-	"id" : 2,
-	"parent" : 1,
-	"children" : []
-}, {
-	"id" : 3,
-	"parent" : 1,
-	"children" : []
-}, {
-	"id" : 4,
-	"parent" : 2,
-	"children" : []
-}, {
-	"id" : 5,
-	"parent" : 2,
-	"children" : []
-}, {
-	"id" : 6,
+	"id" : "c",
 	"parent" : 0,
 	"children" : []
 }, {
-	"id" : 7,
-	"parent" : 0,
+	"id" : "bc",
+	"parent" : "b",
 	"children" : []
 }, {
-	"id" : 8,
-	"parent" : 7,
+	"id" : "bd",
+	"parent" : "b",
 	"children" : []
 }, {
-	"id" : 9,
-	"parent" : 8,
+	"id" : "cd",
+	"parent" : "c",
 	"children" : []
 }, {
-	"id" : 10,
-	"parent" : 0,
+	"id" : "cda",
+	"parent" : "cd",
 	"children" : []
 } ];
 //(function(){
